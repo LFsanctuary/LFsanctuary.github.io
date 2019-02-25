c57169f9ba25165e8f49602b4c69ecaf07602edb
Brittny Lapierre <brittny.lapierre@gmail.com>
2019-02-24 19:08:18 -0500

    active navs

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,11 @@
 loadPage = (pageId) => {
     $("#nav").show();
+    $( ".page-link" ).each(function( index ) {
+        console.log( index + ": " + $( this ).text() );
+        $( this ).removeClass("active");
+    });
+    $("#" + pageId + "-link").addClass("active");
+
     $(".page").each(function( index ) {
         console.log( index + ": " + $( this ).text() );
         $( this ).hide();
