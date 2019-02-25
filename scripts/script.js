872931987153ae1dffaddc0e4c20e3f2429dc7bc
Brittny Lapierre <brittny.lapierre@gmail.com>
2019-02-24 22:29:11 -0500

    new title

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,7 @@
 loadPage = (pageId) => {
-    document.title = 'LF Sanctuary for Women';
+    let title = 'LF Sanctuary for Women';
+    document.title = title;
+    $(document).attr("title", title);
     $("#nav").css("display", "inline-flex");
     $( ".nav-page-link" ).each(function( index ) {
         console.log( index + ": " + $( this ).text() );
