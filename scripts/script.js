7de50cf4462c643eba488d769171727520913dda
Brittny Lapierre <brittny.lapierre@gmail.com>
2019-04-09 22:24:15 -0400

    passcode warning

--- scripts/script.js
+++ scripts/script.js
@@ -2,13 +2,14 @@ loadPage = (pageId) => {
     if(pageId == 'home'){
         let password = $( "#exampleInputPassword1" ).val();
         if(password != 7233){
+            alert("Please enter the correct passcode.")
             return;
         }
     }
 
     let title = 'S.A.F.E Sanctuary For Women';
     document.title = title;
-    
+
     $(document).attr("title", title);
     $("#nav").css("display", "inline-flex");
     $( ".nav-page-link" ).each(function( index ) {
